5d520d357a597c159b5b9e77c4728a0ead10836e
Alexey <94680406+Filippov-Alexey@users.noreply.github.com>
2026-07-13 21:29:19 +0500

    Add files via upload

--- variable.py
+++ variable.py
@@ -1,4 +1,6 @@
-import os
+import re
+import socket
+import subprocess
 from pathlib import Path
 import logger
 log=logger.setup_logging()
@@ -25,7 +27,57 @@ for root, dirs, files in os.walk(dirtools):
 
 TITLE="ФиЛиПпОв_"
 
-ports={'get_key':65431,'get_win':65432,'get_display':65434,'get_space':65435}
+def get_excluded_ports():
+    excluded_ranges = []
+    try:
+        result = subprocess.run(
+            ["netsh", "int", "ipv4", "show", "excludedportrange", "protocol=tcp"],
+            capture_output=True,
+            text=True,
+            encoding="cp866",
+        )
+        if result.returncode == 0:
+            matches = re.findall(r"(\d+)\s+(\d+)", result.stdout)
+            for start, end in matches:
+                excluded_ranges.append((int(start), int(end)))
+    except Exception:
+        pass
+    return excluded_ranges
+
+
+def is_port_blocked_by_system(port, excluded_ranges):
+    for start, end in excluded_ranges:
+        if start <= port <= end:
+            return True
+    return False
+
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind(("localhost", port))
+            return False 
+        except socket.error:
+            return True 
+
+
+def find_free_ports(services, start_port=50000):
+    allocated_ports = {}
+    excluded_ranges = get_excluded_ports()
+    current_port = start_port
+
+    for service in services:
+        while True:
+            if not is_port_blocked_by_system(
+                current_port, excluded_ranges
+            ) and not is_port_in_use(current_port):
+                allocated_ports[service] = current_port
+                current_port += 1
+                break
+            current_port += 1  
+    return allocated_ports
+required_services = ["get_key", "get_win", "get_display", "get_space"]
+ports = find_free_ports(required_services, start_port=50000)
 
 desktop={'space':'5', 'x':'3000', 'y':'0', 'interval':'3000'}
 plugins_dir = Path(os.path.expandvars('.')) / "plugins"
@@ -63,8 +115,8 @@ def is_ignored(window_title):
             return True
     return False
 open_one=["C:\\Windows\\system32\\mspaint.exe"]
-win_size={"C:\\Users\\alexey\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe":(7,0,-6,-6),"O:\\program\\Obsidian\\Obsidian.exe":(6,0,-7,-7)}
-
+# win_size={"C:\\Users\\alexey\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe":(7,0,-6,-6),"O:\\program\\Obsidian\\Obsidian.exe":(6,0,-7,-7)}
+win_size={}
 margin=5
 master_factor=0.5
 tile_mode = 'grid'
